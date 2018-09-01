31ba5a7666a4e8d36feca3ed29a82a6eb5ee33ff
lishilin <15045515313@163.com>
2018-09-01 19:59:15 +0800

    网上购书平台

---
+++ src/com/dangdang/javabean/User.java
@@ -0,0 +1,27 @@
+package com.dangdang.javabean;
+
+public class User {
+
+	private int uid;
+	private String username;
+	private String pass;
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	
+}
